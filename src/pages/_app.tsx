f23cd8a4451b6584805de63c567d492535070fc7
Alex Solda <alexsoldaa@gmail.com>
2024-08-09 23:57:34 -0300

    fix: users page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app'
 import '../app/globals.css'
+import { Providers } from '@/app/providers'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  )
 }
 
 export default MyApp
