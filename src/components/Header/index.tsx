2cb997a25eeab61ece29c0df72b4fbfae2437644
Alex Solda <alexsoldaa@gmail.com>
2024-08-10 01:41:00 -0300

    fix: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,25 +3,16 @@
 import { ReactElement } from 'react'
 import Logo from '../Logo'
 import ThemeToggleButton from '../Buttons/ThemeToggleButton'
+import Wrapper from '../Wrapper'
 
 const Header = (): ReactElement => {
   return (
-    <div>header</div>
-    // <Navbar
-    //   maxWidth="xl"
-    //   className="bg:zinc-200 dark:bg-black-900 border-b-1 border-green"
-    // >
-    //   <NavbarContent justify="start" className="px-4 xl:px-0">
-    //     <NavbarItem>
-    //       <Logo />
-    //     </NavbarItem>
-    //   </NavbarContent>
-    //   <NavbarContent justify="end" className="px-4 xl:px-0">
-    //     <NavbarItem>
-    //       <ThemeToggleButton />
-    //     </NavbarItem>
-    //   </NavbarContent>
-    // </Navbar>
+    <header className="w-full h-[4.063rem] sticky top-0 flex items-center bg:zinc-200 dark:bg-black-900 border-b border-green">
+      <Wrapper className="w-full flex items-center justify-between">
+        <Logo />
+        <ThemeToggleButton />
+      </Wrapper>
+    </header>
   )
 }
 
