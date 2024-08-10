14b3cfbf29ba1718687370226db702f679d4f9ca
Alex Solda <alexsoldaa@gmail.com>
2024-08-10 01:12:33 -0300

    fix: remove nextui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,27 @@
 'use client'
 
-import { Navbar, NavbarContent, NavbarItem } from '@nextui-org/react'
 import { ReactElement } from 'react'
 import Logo from '../Logo'
 import ThemeToggleButton from '../Buttons/ThemeToggleButton'
 
 const Header = (): ReactElement => {
   return (
-    <Navbar
-      maxWidth="xl"
-      className="bg:zinc-200 dark:bg-black-900 border-b-1 border-green"
-    >
-      <NavbarContent justify="start" className="px-4 xl:px-0">
-        <NavbarItem>
-          <Logo />
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end" className="px-4 xl:px-0">
-        <NavbarItem>
-          <ThemeToggleButton />
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+    <div>header</div>
+    // <Navbar
+    //   maxWidth="xl"
+    //   className="bg:zinc-200 dark:bg-black-900 border-b-1 border-green"
+    // >
+    //   <NavbarContent justify="start" className="px-4 xl:px-0">
+    //     <NavbarItem>
+    //       <Logo />
+    //     </NavbarItem>
+    //   </NavbarContent>
+    //   <NavbarContent justify="end" className="px-4 xl:px-0">
+    //     <NavbarItem>
+    //       <ThemeToggleButton />
+    //     </NavbarItem>
+    //   </NavbarContent>
+    // </Navbar>
   )
 }
 
